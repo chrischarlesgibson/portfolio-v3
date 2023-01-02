a1d827567b25426a387936afbb4a953a926553bc
Chris Gibson <chrischarlesgibson@gmail.com>
2023-01-02 14:02:26 -0600

    routes in navbar now work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./pages/About";
@@ -18,10 +13,10 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route exact path="/About" component={About} />
-          <Route exact path="/Projects" component={Projects} />
-          <Route exact path="/Resume" component={Resume} />
-          <Route exact path="/Contact" component={Contact} />
+          <Route exact path="/About" element={<About />} />
+          <Route exact path="/Projects" element={<Projects />} />
+          <Route exact path="/Resume" element={<Resume />} />
+          <Route exact path="/Contact" element={<Contact />} />
         </Routes>
       </div>
       <Footer />
